0aa4a0e57f8bc62f12f9d1c6560c0223d1821c91
Luis F. Vega <vegaluis1220@gmail.com>
2022-08-26 20:27:54 -0400

    Updates

---
+++ precache-manifest.9d43d586d1273ea873acc72bdcf64a46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bc4a3b09610b91d9e9d",
+    "url": "/static/css/main.6f55d95c.chunk.css"
+  },
+  {
+    "revision": "1bc4a3b09610b91d9e9d",
+    "url": "/static/js/main.1bc4a3b0.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "299faf9390feffb5943c",
+    "url": "/static/js/runtime~main.299faf93.js"
+  },
+  {
+    "revision": "6c026c51b00fe7a3eda3e36a8e9556c0",
+    "url": "/index.html"
+  }
+];
